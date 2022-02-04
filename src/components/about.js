fc18397e14c510898f44ae5b83b45ba9d53df365
paweu-gith <64502920+paweu-gith@users.noreply.github.com>
2022-02-04 17:09:41 +0100

    a

--- src/components/about.js
+++ src/components/about.js
@@ -4,14 +4,14 @@ const About = () => {
   return (
     <div
       style={{
-        textAlign: 'left',
+
     padding:"10px",
     paddingBottom:"60px"   
       }}
     >
       
       <h1>Informacje</h1>
-      <p>Aplikacja wykonana w ramach pracy dyplomowej. Zadaniem aplikacji jest umożliwienie zakupu biletu na seans kinowy.<br/>
+      <p style={{textAlign: 'left'}}>Aplikacja wykonana w ramach pracy dyplomowej. Zadaniem aplikacji jest umożliwienie zakupu biletu na seans kinowy.<br/>
       Treść strony jest tylko demonstracyjna. Źródłem plakatów jest strona https://www.filmweb.pl/ <br /><br />
       
       Autor: Paweł Gielej,<br />
